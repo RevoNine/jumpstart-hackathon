46e77762e3ebe019c68a3a001a22748bbaa34bcc
Alexcalibur <calex.corbin@gmail.com>
2025-09-27 11:41:01 +0100

    added step class

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 import pygame_gui
-
+import sys
 
 pygame.init()
 
@@ -10,30 +10,87 @@ window_surface = pygame.display.set_mode((800, 600))
 background = pygame.Surface((800, 600))
 background.fill(pygame.Color('#000000'))
 
-manager = pygame_gui.UIManager((800, 600))
 
-hello_button = pygame_gui.elements.UIButton(
-    relative_rect=pygame.Rect((350, 275), (100, 50)),
-    text='Say Hello',
-    manager=manager
-)
+class Step:
+    def __init__(self):
+        self.manager = pygame_gui.UIManager((800, 600))
+    
+    def run(self, window_surface: pygame.Surface, delta: float) -> bool:
+        return False
+
+
+class Continue(Step):
+    def __init__(self, i):
+        super().__init__()
+        self.done = False
+        self.continue_btn = pygame_gui.elements.UIButton(
+            relative_rect=pygame.Rect((350, 275), (100, 50)),
+            text='Continue',
+            manager=self.manager,
+        );
+        self.i = i
+
+    def run(self, window_surface, delta):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+                
+            self.manager.process_events(event)
+
+            if event.type == pygame_gui.UI_BUTTON_PRESSED:
+                # UI Events
+                if event.ui_element == self.continue_btn:
+                    self.done = True
+                    print('Continuing ' + str(self.i))
+
+        self.manager.update(delta)
+
+        self.manager.draw_ui(window_surface)
+        return self.done
+
 
 clock = pygame.time.Clock()
-is_running = True
 
-while is_running:
+step = 0
+steps = [
+    Continue(0),
+    Continue(1),
+    Continue(2),
+    Continue(3),
+]
+
+manager = pygame_gui.UIManager((800, 600))
+pygame_gui.elements.UILabel(
+    relative_rect=pygame.Rect((350, 275), (100, 50)),
+    text='Well Done!!',
+    manager=manager,
+)
+pygame_gui.elements.UILabel(
+    relative_rect=pygame.Rect((300, 325), (200, 50)),
+    text='Now please Login',
+    manager=manager,
+)
+
+while True:
     time_delta = clock.tick(60)/1000.0
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            is_running = False
-
-        manager.process_events(event)
-
-    manager.update(time_delta)
 
     window_surface.blit(background, (0, 0))
-    manager.draw_ui(window_surface)
+
+    if step < len(steps):
+        if steps[step].run(window_surface, time_delta):
+            step += 1
+    else:
+        manager.update(time_delta)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            
+            manager.process_events(event)
+
+        manager.draw_ui(window_surface)
 
     pygame.display.update()
 
-pygame.quit()
